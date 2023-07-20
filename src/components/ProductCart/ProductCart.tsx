859227f8f1949dfedef29553c71b4d25fd1e4722
Andrii <andriy30534>
2023-07-20 10:28:36 +0300

    added icon basket

--- src/components/ProductCart/ProductCart.tsx
+++ src/components/ProductCart/ProductCart.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react'
 import { AiOutlineHeart } from 'react-icons/ai'
+import { MdShoppingCart } from 'react-icons/md'
 import { Link, useNavigate } from 'react-router-dom'
+import { ProductResponse } from '../../Type/Type'
 import { useAppDispatch, useAppSelector } from '../../hook/reduxHook'
+import { addItemToCart } from '../../store/CartStor/addProductCart'
 import { favoriteProduct, favoriteProductDelete } from '../../store/Product/favoriteSlice'
 import { favorite } from '../../store/Product/getProducts'
-import { ProductResponse } from '../../store/Type'
 import s from './ProductCart.module.scss'
 type Props = {
 	data: ProductResponse
@@ -30,6 +32,11 @@ export const ProductCart: FC<Props> = ({ data }) => {
 		}
 	}
 
+	const addToCartClick: React.MouseEventHandler<HTMLDivElement> = event => {
+		event.preventDefault()
+		dispatch(addItemToCart({ ...data, quantity: 1, totalPrice: data.price }))
+	}
+
 	return (
 		<Link className={s.text} to={`/product/${data.id}`}>
 			<div className={s.wrapper}>
@@ -49,6 +56,10 @@ export const ProductCart: FC<Props> = ({ data }) => {
 					<div onClick={handleProduct} className={s.cart__like}>
 						<AiOutlineHeart className={data.favorite ? s.hart : ''} />
 					</div>
+					<div onClick={addToCartClick} className={s.cart}>
+						<MdShoppingCart className={''} />
+					</div>
+
 					<h2 className={s.cart__title}>{data.title}</h2>
 					<div className={s.cart__price}>
 						<> $ {data.price}</>
